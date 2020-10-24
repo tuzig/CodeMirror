8c233dae49e7495e94b6a3acf2fd73af03b8cad8
Benny Daon <benny@tuzig.com>
2020-10-24 19:10:23 +0300

    es6-ing the dialog addon

--- addon/dialog/dialog.js
+++ addon/dialog/dialog.js
@@ -3,14 +3,7 @@
 
 // Open simple dialogs on top of an editor. Relies on dialog.css.
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
+export function dialogAddOn(CodeMirror) {
   function dialogDiv(cm, template, bottom) {
     var wrap = cm.getWrapperElement();
     var dialog;
@@ -160,4 +153,4 @@
 
     return close;
   });
-});
+};
