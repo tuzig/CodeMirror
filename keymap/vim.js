20d100cfe01110f80d079e349252a8bc74999262
Benny Daon <benny@tuzig.com>
2020-10-24 16:14:05 +0300

    Add es6 support for vim & toml
    based on https://github.com/codemirror/CodeMirror/issues/5403

--- keymap/vim.js
+++ keymap/vim.js
@@ -34,14 +34,7 @@
  *  9. Ex command implementations.
  */
 
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../lib/codemirror"), require("../addon/search/searchcursor"), require("../addon/dialog/dialog"), require("../addon/edit/matchbrackets.js"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../lib/codemirror", "../addon/search/searchcursor", "../addon/dialog/dialog", "../addon/edit/matchbrackets"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
+export function vimMode(CodeMirror) {
   'use strict';
 
   var defaultKeymap = [
@@ -5712,4 +5705,4 @@
   };
   // Initialize Vim and make it available as an API.
   CodeMirror.Vim = Vim();
-});
+};
